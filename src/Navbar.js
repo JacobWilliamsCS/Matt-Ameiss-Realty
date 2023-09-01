4332fb216c966c2e97130fc937773e65776ec5a1
jacob <jacobmiwilliams@gmail.com>
2023-09-01 13:00:50 -0500

    cheeseburger

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './Navbar.css';
 
@@ -6,7 +6,7 @@ import InformationPage from './InformationPage';
 import PropertiesPage from './PropertiesPage';
 
 function Navbar() {
-    const [menu, setMenu] = useState(false);
+   // const [menu, setMenu] = useState(false);
 
     return (
         <div className='flex flex-col' style={{ 'height': '100%' }}>
@@ -20,17 +20,18 @@ function Navbar() {
                                         <h1>Matt Ameiss Realty</h1>
                                     </Link>
                                     
-                                        <Link onClick={() => setMenu(false)} to="/properties" className="tab-button"><h2> Listings</h2></Link>
+                                                {/*onClick={() => setMenu(false)}  */} 
+                                         <Link            to="/properties" className="tab-button"><h2> Listings</h2></Link>
                                         {/* Add more tab buttons here if needed */}
 
                                         {/* Hamburger button */}
                                         {/* <div className="flex items-center justify-between flex-wrap text-m"> */}
-                                    <button onClick={() => setMenu(!menu)} className="md:hidden px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white transform active:scale-95">
+                                    {/* <button onClick={() => setMenu(!menu)} className="md:hidden px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white transform active:scale-95">
                                         <svg viewBox="0 0 100 55" width="20" height="20">
                                             <rect width="100" height="20" fill="#ffffff"></rect>
                                             <rect y="40" width="100" height="20" fill="#ffffff"></rect>
                                         </svg>
-                                    </button> 
+                                    </button>  */}
                                 {/* </div> */}
                             {/* </div> */}
                             {/* </div> */}
