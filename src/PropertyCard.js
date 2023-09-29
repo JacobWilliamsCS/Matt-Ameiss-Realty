f1d95bdbd92c1a9876bc44f11604fa58b7af4181
jacob <jacobmiwilliams@gmail.com>
2023-09-28 19:16:23 -0500

    updated a lot

--- src/PropertyCard.js
+++ src/PropertyCard.js
@@ -1,15 +1,42 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+
 import './PropertyCard.css';
 import './properties'
 
 const PropertyCard = ({ property }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === property.images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const previousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? property.images.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
     <div className="property-card">
-      <img src={property.image} alt={property.title} />
-      <h2>{property.title}</h2>
+      <h2>{property.name}</h2>
       <p>{property.description}</p>
       <p>{property.link}</p>
       <p>Price: {property.price}</p>
+      <div className="image-container">
+        <button className="arrow-button left" onClick={previousImage}>
+          &lt;
+        </button>
+        <img
+          src={property.images[currentImageIndex]}
+          alt={`Image ${currentImageIndex + 1}`}
+        />
+        <button className="arrow-button right" onClick={nextImage}>
+          &gt;
+        </button>
+      </div>
     </div>
   );
 };
--- src/properties.js
+++ src/properties.js
@@ -1,57 +1,74 @@
 const properties = [
-    {
-      id: 1,
-      title: "Historical Home In The City",
-      description: "A classic St. Charles home built in the 19th century.",
-      price: "$350,000",
-      image: "oldhouse.jpg",
-      link: "https://ryserealtystl.com",
-    },
-    {
-      id: 2,
-      title: "Spacious Family Home",
-      description: "A spacious and cozy family home with a beautiful garden and large backyard.",
-      price: "$550,000",
-      image: "https://havenlifestyles.s3.us-east-2.amazonaws.com/Four+Must-See+Stylishly+Modern+Wisconsin+Homes.jpg",
-      link: "https://ryserealtystl.com",
-    },
-    // Add more properties here
-    {
-        id: 3,
-        title: "Brick Cottage",
-        description: "A brand new 3 bedroom 3 bathroom home with a good sized yard.",
-        price: "$485,000",
-        image: "https://sebernhomes.com/wp-content/uploads/2020/06/Mallard-Exterior-Zoom-Out.jpg",
-        link: "https://ryserealtystl.com",
-    },
-    {
-        id: 4,
-        title: "Newly Renovated",
-        description: "A newly renovated home right in the center of O'Fallon.",
-        price: "$250,000",
-        image: "https://ap.rdcpix.com/6f34693be1993ea8a2911e11a400805bl-m598688742od-w480_h360.jpg",
-        link: "https://ryserealtystl.com",
-    },
-    {
-        id: 5,
-        title: "Large Traditional Home",
-        description: "Plenty of wildlife and spacious property.",
-        price: "$429,000",
-        image: "https://www.mydomaine.com/thmb/CaWdFGvTH4-h1VvG6tukpKuU2lM=/3409x0/filters:no_upscale():strip_icc()/binary-4--583f06853df78c6f6a9e0b7a.jpeg",
-        link: "https://ryserealtystl.com",
-    },
-    {
-        id: 6,
-        title: "Enormous House and Perfect Landscape",
-        description: "Built in 2021 this house has everything to do from an inground pool to the man cave.",
-        price: "$775,000",
-        image: "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_24/1448814/how-size-doesnt-make-you-happier-today-main-190614.jpg",
-        link: "https://ryserealtystl.com",
-    }
+  {
+    id: 1,
+    title: "Historical Home In The City",
+    description: "A classic St. Charles home built in the 19th century.",
+    price: "$350,000",
+    images: ["oldhouse.jpg",
+
+  ],
+    link: "https://ryserealtystl.com",
+  },
+  {
+    id: 2,
+    title: "Spacious Family Home",
+    description: "A spacious and cozy family home with a beautiful garden and large backyard.",
+    price: "$550,000",
+    images: ["https://havenlifestyles.s3.us-east-2.amazonaws.com/Four+Must-See+Stylishly+Modern+Wisconsin+Homes.jpg",
+            "https://sebernhomes.com/wp-content/uploads/2020/06/Mallard-Exterior-Zoom-Out.jpg"
+
+  ],
+    link: "https://ryserealtystl.com",
+  },
   // Add more properties here
+  {
+      id: 3,
+      title: "Brick Cottage",
+      description: "A brand new 3 bedroom 3 bathroom home with a good sized yard.",
+      price: "$485,000",
+      images: ["https://sebernhomes.com/wp-content/uploads/2020/06/Mallard-Exterior-Zoom-Out.jpg",
+
+  ],
+      link: "https://ryserealtystl.com",
+  },
+  {
+      id: 4,
+      title: "Newly Renovated",
+      description: "A newly renovated home right in the center of O'Fallon.",
+      price: "$250,000",
+      images: [ "https://ap.rdcpix.com/6f34693be1993ea8a2911e11a400805bl-m598688742od-w480_h360.jpg",
+                "livingroom.jpg",
+                "bath.jpg",
+                "kitchen.jpg"
+
+
+  ],
+      link: "https://ryserealtystl.com",
+  },
+  {
+      id: 5,
+      title: "Large Traditional Home",
+      description: "Plenty of wildlife and spacious property.",
+      price: "$429,000",
+      images: ["https://www.mydomaine.com/thmb/CaWdFGvTH4-h1VvG6tukpKuU2lM=/3409x0/filters:no_upscale():strip_icc()/binary-4--583f06853df78c6f6a9e0b7a.jpeg",
+
+  ],
+      link: "https://ryserealtystl.com",
+  },
+  {
+      id: 6,
+      title: "Enormous House and Perfect Landscape",
+      description: "Built in 2021 this house has everything to do from an inground pool to the man cave.",
+      price: "$775,000",
+      images: ["https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_24/1448814/how-size-doesnt-make-you-happier-today-main-190614.jpg",
+
+
+  ],
+      link: "https://ryserealtystl.com",
+  }
+// Add more properties here
 
 
 ]
-  
-  export default properties;
-  
+
+export default properties;
