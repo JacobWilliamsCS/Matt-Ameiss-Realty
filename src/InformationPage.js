8866bbe0282775a5eb75d85a8725bb854d754025
jacob <jacobmiwilliams@gmail.com>
2023-10-10 00:11:37 -0500

    updated home page

--- src/InformationPage.js
+++ src/InformationPage.js
@@ -6,14 +6,15 @@ function InformationPage() {
 
   //array of descriptions
   const descriptions = [
+
     "Navigating through your most significant transactions becomes an effortless journey with the guidance of our esteemed advisor, Matt. His innate kindness, unwavering patience, and genuine enthusiasm are all dedicated to ensuring that his clients not only experience a stress-free process but also attain a crystal-clear comprehension of every facet within the realms of buying, selling, or even building.",
     "If you're seeking an advisor who embodies the art of listening and places your aspirations above all else, reaching out to Matt is a step towards a partnership that transcends the ordinary. Don't hesitate to connect with Matt today and embark on a real estate journey that's both exceptional and deeply personalized.",
    
     "Discover effortless transactions with Matt by your side. His genuine care, patience, and expertise are devoted to providing you with a smooth and transparent journey, whether you're buying, selling, or embarking on a construction project.",
     "Experience the art of real estate with Matt's dedicated guidance. His kindness, unmatched patience, and enthusiasm are your keys to a hassle-free process and a clear understanding of the intricacies involved in real estate transactions.",
 
-    "Make your real estate endeavors a joy with Matt's expert counsel. His commitment, patience, and passion ensure that you not only achieve a stress-free experience but also gain a profound understanding of buying, selling, or building in the real estate realm.",
-    "With Matt as your trusted real estate advisor, navigating important transactions is a breeze. His commitment to excellence, unwavering support, and passion for real estate ensure that you not only enjoy a stress-free experience but also gain clarity on all aspects of buying, selling, or building your property."
+  
+    
 
   ];
 
@@ -32,16 +33,25 @@ function InformationPage() {
         <img src="facebooklogo.png" alt="Facebook Logo" style={{ width: '50px', height: '50px'}}/>
         </a>
         </div>
-        <div className="description-card white px-4">
+
+        
+
+        <div className="testimonial-container" style={{ display: 'flex', flexWrap: 'wrap' }}></div>
         {/* <h2>Customer Testimonials</h2> */}
         {/* Render description cards */}
         {descriptions.map((description, index) => (
           <div className="description-card" key={index}>
             <p>{description}</p>
+
+            
           </div>
+         
         ))}
+
+
+
       </div>
-    </div>
+ 
    
     
 
