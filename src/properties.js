f075d65fee2a63f8c3827c94f56f232beb28b8cd
jacob <jacobmiwilliams@gmail.com>
2023-10-10 00:35:23 -0500

    updated homes sold page

--- src/properties.js
+++ src/properties.js
@@ -7,64 +7,66 @@ const properties = [
     images: ["oldhouse.jpg",
 
   ],
-    link: "https://ryserealtystl.com",
+    link: "4 Beds | 2 Bath | 1675 sq. ft.",
   },
   {
     id: 2,
     title: "Spacious Family Home",
     description: "A spacious and cozy family home with a beautiful garden and large backyard.",
-    price: "$550,000",
+    price: "$549,000",
     images: ["https://havenlifestyles.s3.us-east-2.amazonaws.com/Four+Must-See+Stylishly+Modern+Wisconsin+Homes.jpg",
-            "https://sebernhomes.com/wp-content/uploads/2020/06/Mallard-Exterior-Zoom-Out.jpg"
+            "bath1.jpg",
+            "mancave.jpg",
 
   ],
-    link: "https://ryserealtystl.com",
+    link: "6 Beds | 5 Bath | 2589 sq. ft.",
   },
   // Add more properties here
   {
       id: 3,
       title: "Brick Cottage",
       description: "A brand new 3 bedroom 3 bathroom home with a good sized yard.",
-      price: "$485,000",
+      price: "$569,000",
       images: ["https://sebernhomes.com/wp-content/uploads/2020/06/Mallard-Exterior-Zoom-Out.jpg",
 
   ],
-      link: "https://ryserealtystl.com",
+      link: "8 Beds | 6 Bath | 2188 sq. ft.",
   },
   {
       id: 4,
       title: "Newly Renovated",
       description: "A newly renovated home right in the center of O'Fallon.",
-      price: "$250,000",
+      price: "$275,000",
       images: [ "https://ap.rdcpix.com/6f34693be1993ea8a2911e11a400805bl-m598688742od-w480_h360.jpg",
-                "livingroom.jpg",
-                "bath.jpg",
-                "kitchen.jpg"
+                "bath1.jpg",
+                "kitchen0.jpg"
 
 
   ],
-      link: "https://ryserealtystl.com",
+      link: "3 Beds | 3 Bath | 1602 sq. ft.",
   },
   {
       id: 5,
       title: "Large Traditional Home",
       description: "Plenty of wildlife and spacious property.",
-      price: "$429,000",
+      price: "$405,000",
       images: ["https://www.mydomaine.com/thmb/CaWdFGvTH4-h1VvG6tukpKuU2lM=/3409x0/filters:no_upscale():strip_icc()/binary-4--583f06853df78c6f6a9e0b7a.jpeg",
+      "bath2.jpg",
 
   ],
-      link: "https://ryserealtystl.com",
+      link: "5 Beds | 3 Bath | 1770 sq. ft.",
   },
   {
       id: 6,
       title: "Enormous House and Perfect Landscape",
       description: "Built in 2021 this house has everything to do from an inground pool to the man cave.",
-      price: "$775,000",
+      price: "$915,000",
       images: ["https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_24/1448814/how-size-doesnt-make-you-happier-today-main-190614.jpg",
+      "mancave2.jpg",
 
 
   ],
-      link: "https://ryserealtystl.com",
+      link: "10 Beds | 7 Bath | 2795 sq. ft.",
   }
 // Add more properties here
 
